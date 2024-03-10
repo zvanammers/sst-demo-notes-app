1d8abcd618d61e0f7fa58cfd7280040ea067bd0c
zvanammers <50008992+zvanammers@users.noreply.github.com>
2024-03-10 16:50:36 +1100

    feat: create new apis for dynamodb table (#2)

---
+++ notes/packages/functions/src/create.ts
@@ -0,0 +1,30 @@
+import { randomUUID } from "crypto";
+import { Table } from "sst/node/table";
+import dynamoDb from "@notes/core/dynamodb";
+import handler from "@notes/core/handler";
+
+export const main = handler(async (event) => {
+    let data = {
+        content: "",
+        attachment: "",
+    };
+
+    if (event.body != null) {
+        data = JSON.parse(event.body)
+    }
+
+    const params = {
+        TableName: Table.Notes.tableName,
+        Item: {
+            userId: "123",
+            noteId: randomUUID(),
+            content: data.content,
+            attachment: data.attachment,
+            createAt: Date.now(),
+        },
+    };
+
+    await dynamoDb.put(params);
+
+    return JSON.stringify(params.Item);
+})
---
+++ notes/packages/functions/src/delete.ts
@@ -0,0 +1,19 @@
+import { Table } from "sst/node/table";
+import dynamoDb from "@notes/core/dynamodb";
+import handler from "@notes/core/handler";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+export const main = handler(async (event) => {
+
+    const deleteParams: DocumentClient.DeleteItemInput = {
+        TableName: Table.Notes.tableName,
+        Key: {
+            userId: "123",
+            noteId: event?.pathParameters?.id
+        },
+    };
+
+    await dynamoDb.delete(deleteParams);
+
+    return JSON.stringify({status: true});
+})
