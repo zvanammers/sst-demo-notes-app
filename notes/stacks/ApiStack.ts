b5041254b7e1417fcd11e751000915e2d1178d49
zvanammers <50008992+zvanammers@users.noreply.github.com>
2024-03-17 15:48:52 +1100

    feat: weather API (#4)
    * feat: weather API
    
    * fix: remove comments

--- notes/stacks/ApiStack.ts
+++ notes/stacks/ApiStack.ts
@@ -1,14 +1,16 @@
-import { Api, StackContext, use } from "sst/constructs";
+import { Api, Config, StackContext, use } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 
 export function ApiStack({ stack }: StackContext) {
     const { table } = use(StorageStack);
 
+    const WEATHER_API_SECRET_KEY = new Config.Secret(stack, "WEATHER_API_SECRET_KEY");
+
     const api = new Api(stack, "Api", {
         defaults: {
             authorizer: "iam",
             function: {
-                bind: [table],
+                bind: [table, WEATHER_API_SECRET_KEY],
             }
         },
         routes: {
@@ -16,7 +18,8 @@ export function ApiStack({ stack }: StackContext) {
             "GET /notes/{id}": "packages/functions/src/get.main",
             "GET /notes": "packages/functions/src/list.main",
             "PUT /notes/{id}": "packages/functions/src/update.main",
-            "DELETE /notes/{id}": "packages/functions/src/delete.main"
+            "DELETE /notes/{id}": "packages/functions/src/delete.main",
+            "GET /weather": "packages/functions/src/weather.main",
         },
     });
     stack.addOutputs({
