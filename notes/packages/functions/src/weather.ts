6ae95b41ce697a9cc0d79604ff1f3b009c8bb01a
Zoe van Ammers <johannacows52@gmail.com>
2024-05-04 12:24:02 +1000

    feat: replace eslint with biomejs

--- notes/packages/functions/src/weather.ts
+++ notes/packages/functions/src/weather.ts
@@ -1,16 +1,27 @@
 import handler from '@notes/core/handler';
 import fetch from 'node-fetch';
 import { Config } from 'sst/node/config';
-import currentWeather, { popularWeatherStats } from '../../core/models/currentWeather';
+import currentWeather, {
+	popularWeatherStats,
+} from '../../core/models/currentWeather';
 
 export const main = handler(async (event) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { lat, lon } = JSON.parse(event.body || "{}");
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	let { lat, lon } = JSON.parse(event.body || '{}');
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${Config.WEATHER_API_SECRET_KEY}`);
+	if (lat == null) {
+		lat = '-38.15';
+	}
+	if (lon == null) {
+		lon = '144.34';
+	}
 
-    const body = await response.json() as currentWeather;
-    const temps = body.main as popularWeatherStats;
+	const response = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${Config.WEATHER_API_SECRET_KEY}`,
+	);
 
-    return JSON.stringify(temps);
-})
+	const body = (await response.json()) as currentWeather;
+	const temps = body.main as popularWeatherStats;
+
+	return JSON.stringify(temps);
+});
--- notes/sst.config.ts
+++ notes/sst.config.ts
@@ -1,17 +1,17 @@
-import { SSTConfig } from "sst";
-import { StorageStack } from "./stacks/StorageStack";
-import { ApiStack } from "./stacks/ApiStack";
-import { AuthStack } from "./stacks/AuthStack";
+import { SSTConfig } from 'sst';
+import { StorageStack } from './stacks/StorageStack';
+import { ApiStack } from './stacks/ApiStack';
+import { AuthStack } from './stacks/AuthStack';
 
 export default {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  config(_input) {
-    return {
-      name: "notes",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
-  }
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	config(_input) {
+		return {
+			name: 'notes',
+			region: 'us-east-1',
+		};
+	},
+	stacks(app) {
+		app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+	},
 } satisfies SSTConfig;
