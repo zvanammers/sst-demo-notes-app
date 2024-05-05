0b43034280e1e175b273cc0af98c8a8001f12b91
Zoe van Ammers <johannacows52@gmail.com>
2024-05-05 10:54:35 +1000

    fix: layout

--- notes/packages/frontend/src/routes/__root.tsx
+++ notes/packages/frontend/src/routes/__root.tsx
@@ -1,63 +1,3 @@
-// import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
-// import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-// import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
-// import type { MenuProps } from 'antd';
-// import { Menu } from 'antd';
-// import { useState } from 'react';
-// import App from '../App';
-
-// type MenuItem = Required<MenuProps>['items'][number];
-
-// const items: MenuItem[] = [
-// 	{
-// 		label: 'Navigation One',
-// 		key: 'mail',
-// 		icon: <MailOutlined />,
-// 	},
-// 	{
-// 		label: 'Navigation Two',
-// 		key: 'app',
-// 		icon: <AppstoreOutlined />,
-// 	},
-// 	{
-// 		key: 'About',
-// 		label: (
-// 			<Link to="/about" className="[&.active]:font-bold">
-// 				About
-// 			</Link>
-// 		),
-// 	},
-// ];
-
-// const [current, setCurrent] = useState('mail');
-
-// const onClick: MenuProps['onClick'] = (e) => {
-// 	setCurrent(e.key);
-// };
-
-// export const Route = createRootRoute({
-// 	component: () => (
-// 		<>
-// 			<App />
-// 			{/* <div className="p-2 flex gap-2">
-// 				<Link to="/" className="[&.active]:font-bold">
-// 					Home
-// 				</Link>{' '}
-// 				<Link to="/about" className="[&.active]:font-bold">
-// 					About
-// 				</Link>
-// 			</div> */}
-// 			<hr />
-// 			<Outlet />
-// 			<TanStackRouterDevtools />
-// 		</>
-// 	),
-// });
-
-export const Route = createRootRoute({
-	component: App,
-});
-
 import '../App.css';
 import { useState } from 'react';
 import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
@@ -67,6 +7,10 @@ const { Header, Content, Footer, Sider } = Layout;
 import { Link } from '@tanstack/react-router';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+export const Route = createRootRoute({
+	component: App,
+});
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -103,42 +47,6 @@ function App() {
 	} = theme.useToken();
 
 	return (
-		// <Flex gap="middle" wrap>
-		// 	<Layout style={layoutStyle}>
-		// 		<Header style={headerStyle}>
-		// 			<div className="demo-logo" />
-		// 			<Menu
-		// 				onClick={onClick}
-		// 				selectedKeys={[current]}
-		// 				mode="horizontal"
-		// 				items={items}
-		// 				theme="dark"
-		// 			/>
-		// 		</Header>
-		// 		<Content style={contentStyle}>
-		// 			<Content style={{ padding: '0 48px' }}>
-		// 				<Breadcrumb style={{ margin: '16px 0' }}>
-		// 					<Breadcrumb.Item>Home</Breadcrumb.Item>
-		// 					<Breadcrumb.Item>List</Breadcrumb.Item>
-		// 					<Breadcrumb.Item>App</Breadcrumb.Item>
-		// 				</Breadcrumb>
-		// 				<div
-		// 					style={{
-		// 						background: colorBgContainer,
-		// 						minHeight: 280,
-		// 						padding: 24,
-		// 						borderRadius: borderRadiusLG,
-		// 					}}
-		// 				>
-		// 					<hr />
-		// 					<Outlet />
-		// 					<TanStackRouterDevtools />
-		// 				</div>
-		// 			</Content>
-		// 		</Content>
-		// 		<Footer style={footerStyle}>Footer</Footer>
-		// 	</Layout>
-		// </Flex>
 		<Layout style={{ minWidth: '100vh' }}>
 			<Header style={{ display: 'flex', alignItems: 'center' }}>
 				<div className="demo-logo" />
@@ -163,7 +71,6 @@ function App() {
 						minHeight: '85vh',
 						padding: 24,
 						borderRadius: borderRadiusLG,
-						minWidth: '85vh',
 					}}
 				>
 					<hr />
