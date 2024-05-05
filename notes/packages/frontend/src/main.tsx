322c2c37267ec9c96a0622bdf147d7c9481c3f32
zvanammers <50008992+zvanammers@users.noreply.github.com>
2024-05-05 11:24:45 +1000

    feat: add routing and css (#9)

--- notes/packages/frontend/src/main.tsx
+++ notes/packages/frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+const rootElement = document.getElementById('app')!;
+console.log(rootElement);
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
---
+++ notes/packages/frontend/src/routes/__root.tsx
@@ -0,0 +1,78 @@
+import '../App.css';
+import { useState } from 'react';
+import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Flex } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+import { Link } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+export const Route = createRootRoute({
+	component: App,
+});
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function App() {
+	const items: MenuItem[] = [
+		{
+			key: 'Home',
+			label: (
+				<Link to="/" className="[&.active]:font-bold">
+					Home
+				</Link>
+			),
+			icon: <AppstoreOutlined />,
+		},
+		{
+			key: 'About',
+			label: (
+				<Link to="/about" className="[&.active]:font-bold">
+					About
+				</Link>
+			),
+			icon: <MailOutlined />,
+		},
+	];
+
+	const [current, setCurrent] = useState('mail');
+
+	const onClick: MenuProps['onClick'] = (e) => {
+		setCurrent(e.key);
+	};
+
+	const {
+		token: { colorBgContainer, borderRadiusLG },
+	} = theme.useToken();
+
+	return (
+		<Layout>
+			<Header style={{ display: 'flex', alignItems: 'center' }}>
+				<div className="demo-logo" />
+				<Menu
+					onClick={onClick}
+					selectedKeys={[current]}
+					mode="horizontal"
+					items={items}
+					theme="dark"
+					style={{ flex: 1, minWidth: 0 }}
+				/>
+			</Header>
+			<Content style={{ padding: '48px 48px' }}>
+				<div
+					style={{
+						background: colorBgContainer,
+						minHeight: '85vh',
+						padding: 24,
+						borderRadius: borderRadiusLG,
+					}}
+				>
+					<Outlet />
+					<TanStackRouterDevtools />
+				</div>
+			</Content>
+			<Footer style={{ textAlign: 'center' }}>Created by Zoe van Ammers</Footer>
+		</Layout>
+	);
+}
