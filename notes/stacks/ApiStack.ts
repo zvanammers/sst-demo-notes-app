e3c8ab5d3f3579b7a1a11c066ecb012e1534f2b0
zvanammers <50008992+zvanammers@users.noreply.github.com>
2024-05-26 19:10:07 +1000

    feat: add cors allow origins (#18)
    * feat: add cors allow origins
    
    * fix: remove comment
    
    * fix: tests

--- notes/stacks/ApiStack.ts
+++ notes/stacks/ApiStack.ts
@@ -12,6 +12,7 @@ export function ApiStack({ stack }: StackContext) {
 	const api = new Api(stack, 'Api', {
 		cors: {
 			allowMethods: ['GET'],
+			allowOrigins: ['https://d1jrqg0cz9i57u.cloudfront.net'],
 		},
 		defaults: {
 			// authorizer: "iam",
@@ -20,11 +21,6 @@ export function ApiStack({ stack }: StackContext) {
 			},
 		},
 		routes: {
-			'POST /notes': 'packages/functions/src/create.main',
-			'GET /notes/{id}': 'packages/functions/src/get.main',
-			'GET /notes': 'packages/functions/src/list.main',
-			'PUT /notes/{id}': 'packages/functions/src/update.main',
-			'DELETE /notes/{id}': 'packages/functions/src/delete.main',
 			'GET /weather': 'packages/functions/src/weather.main',
 		},
 	});
